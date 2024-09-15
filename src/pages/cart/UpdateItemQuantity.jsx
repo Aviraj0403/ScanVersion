b85f6b6c26740fa81f80ddc42b4ca7b37c822d9d
aviraj0403 <aviraj0403@gmail.com>
2024-09-15 23:38:01 +0530

    Rename `foodItemId` to `fooditemId` in `UpdateItemQuantity` component props.

--- src/pages/cart/UpdateItemQuantity.jsx
+++ src/pages/cart/UpdateItemQuantity.jsx
@@ -1,21 +1,21 @@
 import { useDispatch } from "react-redux";
 import { increaseItem, decreaseItem } from "../cart/cartSlice.js";
 
-const UpdateItemQuantity = ({ foodItemId, quantity }) => {
+const UpdateItemQuantity = ({ fooditemId, quantity }) => {
   const dispatch = useDispatch();
 
   return (
     <div className="flex items-center justify-center gap-2">
       <button
         className="rounded-lg bg-gray-50 px-3 pb-1 pt-0.5 text-xl text-gray-600 transition-all hover:bg-gray-100"
-        onClick={() => dispatch(decreaseItem(foodItemId))}
+        onClick={() => dispatch(decreaseItem(fooditemId))}
       >
         -
       </button>
       <div className="mx-2">{quantity}</div>
       <button
         className="rounded-lg bg-orange-400 px-3 pb-1 pt-0.5 text-xl text-white transition-all hover:bg-orange-500"
-        onClick={() => dispatch(increaseItem(foodItemId))}
+        onClick={() => dispatch(increaseItem(fooditemId))}
       >
         +
       </button>
